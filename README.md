89610ead30c4ffb41a755398574dc28a154a39d4
Andreas Pardeike <andreas@pardeike.net>
2022-10-13 03:27:40 +0200

    Add project files.

---
+++ README.md
@@ -0,0 +1,13 @@
+# LagFix
+
+#### Removes the baby food lag in 1.4
+
+Patches away the alerts telling you about baby food problems on your map. Because those checks are super heavy.
+
+### License
+
+Free. As in free beer. Copy, learn and be respectful.
+
+### Author
+
+Andreas "Brrainz" Pardeike
---
+++ Source/Main.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace LagFix
+{
+	public class LagFix_Main : Mod
+	{
+		public LagFix_Main(ModContentPack content) : base(content)
+		{
+			var harmony = new Harmony("net.pardeike.lagfix");
+			harmony.PatchAll();
+		}
+	}
+
+	[HarmonyPatch(typeof(Alert_NoBabyFeeders), nameof(Alert_NoBabyFeeders.MapWithNoBabyFeeder))]
+	static class Alert_NoBabyFeeders_MapWithNoBabyFeeder_Patch
+	{
+		static bool Prefix(ref Map __result)
+		{
+			__result = null;
+			return false;
+		}
+	}
+
+	[HarmonyPatch(typeof(Alert_LowBabyFood), nameof(Alert_LowBabyFood.LowBabyFoodNutrition))]
+	static class Alert_LowBabyFood_LowBabyFoodNutrition_Patch
+	{
+		static bool Prefix(ref bool __result)
+		{
+			__result = false;
+			return false;
+		}
+	}
+}
